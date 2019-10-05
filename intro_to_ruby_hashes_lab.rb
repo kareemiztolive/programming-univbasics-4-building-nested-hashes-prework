21765b740606d13797fb2da58962b91557502682
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 00:31:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,9 @@ end
 def monopoly_with_third_tier
  monopoly_with_third_tier = {
                             :railroads => {:pieces => 4,
-                                           :rent_in_dollars => {:one_piece_owned}
+                                           :rent_in_dollars => {:one_piece_owned => 25
+                                                                :
+                                           }
                             
                             } 
                             }
