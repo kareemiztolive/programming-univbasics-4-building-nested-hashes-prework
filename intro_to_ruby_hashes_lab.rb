49550d911014213976920fd04f0b174499fc8e6a
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 01:31:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
                                                                 :three_pieces_owned => 100,
                                                                 :four_pieces_owned => 200
                                            },
-                                           :names => {:reading_railroad => {""},                                                          :pennsylvania_railroad => {},
+                                           :names => {:reading_railroad => {"mortgage_value" => },                                            :pennsylvania_railroad => {},
                                                        :b_and_o_railroad => {},
                                                        :shortline_railroad => {}
                             
