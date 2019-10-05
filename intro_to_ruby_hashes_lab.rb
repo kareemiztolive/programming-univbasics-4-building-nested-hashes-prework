9fdf5e37e80f503781ad96ed0fff61d21fb7bc1d
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 01:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,8 @@ def monopoly_with_third_tier
  monopoly_with_third_tier = {
                             :railroads => {:pieces => 4,
                                            :rent_in_dollars => {:one_piece_owned => 25,
-                                                                :two_pieces_owned => 75,
-                                                                :three_pieces_owned => 50,
+                                                                :two_pieces_owned => 50,
+                                                                :three_pieces_owned => 100,
                                                                 :four_pieces_owned => 200
                                            },
                                            :names => {:reading_railroad => {},                                                          :pennsylvania_railroad => {},
@@ -22,7 +22,8 @@ def monopoly_with_third_tier
                             
                             } 
                             }
- }
+ 
+}
                             
                                
 
